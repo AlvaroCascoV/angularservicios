a20342cb739bdf3354051791bc38f82e67274063
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-11-07 11:39:49 +0100

    inicio standalone

---
+++ src/app/components/personas-standalone-component/personas-standalone-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personas-standalone-component',
+  standalone: true,
+  templateUrl: './personas-standalone-component.html',
+  styleUrl: './personas-standalone-component.css',
+})
+export class PersonasStandaloneComponent {}
