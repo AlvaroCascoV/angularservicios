0024a55a3c502cdf4753eb97b9732a022cad079d
AlvaroCascoV <alvarocascovalero@gmail.com>
2025-11-11 10:09:34 +0100

    quick comment

--- src/app/services/service.coches.ts
+++ src/app/services/service.coches.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class ServiceCoches {
-  constructor(private _http: HttpClient) {}
+  // constructor(private _http: HttpClient) {}
 
   // getCochesHttpClient(): Observable<any> {
   //   let request = 'webresources/coches';
